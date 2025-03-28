f2f4faffda16fbdcbee2f3879edc98731b6c5d12
sothearo-kay <sothearo.kay@techostartup.center>
2025-03-28 09:16:19 +0700

    solution to 2020 day 2: Password Philosophy

--- 2020/2.cpp
+++ 2020/2.cpp
@@ -68,7 +68,32 @@ int main() {
     }
   }
 
-  cout << validCount << endl;
+  // ---- Part 2
+  /*
+     Each policy actually describes two positions in the password, where 1 means
+     the first character, 2 means the second character, and so on. (Be careful;
+     Toboggan Corporate Policies have no concept of "index zero"!) Exactly one
+     of these positions must contain the given letter. Other occurrences of the
+     letter are irrelevant for the purposes of policy enforcement.
+
+     Given the same example list from above:
+
+     - 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+     - 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+     - 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+    */
+
+  for (const auto &entry : entries) {
+    bool firstPos = entry.password[entry.min - 1] == entry.letter;
+    bool secondPos = entry.password[entry.max - 1] == entry.letter;
+
+    // XOR ensures exactly one of them is true
+    if (firstPos ^ secondPos) {
+      validCount++;
+    }
+  }
+
+  // cout << validCount << endl;
 
   return 0;
 }
